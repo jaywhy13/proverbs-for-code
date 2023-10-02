d3ea442f4b1b6f49b21131153148febcfe7b3d36
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-10-01 23:00:10 -0400

    Added tests for the SuggestedProverb component.
    It verifies that the different visual components are
    present and that the button works as expected.

---
+++ src/components/SuggestedProverb.spec.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, getByText, fireEvent } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
+import { SuggestedProverb, SuggestedProverbProps } from "./SuggestedProverb";
+import { v4 } from "uuid";
+
+describe("SuggestedProverb", () => {
+  const defaultProps: SuggestedProverbProps = {
+    id: v4(),
+    proverb: "This is the proverb",
+    meaning: "This be the meaning",
+    relation: "This is the relation",
+    onRemove: jest.fn(),
+  };
+
+  it("should show the proverb", () => {
+    render(<SuggestedProverb {...defaultProps} />);
+    const { proverb } = defaultProps;
+    expect(screen.getByText(proverb)).toBeInTheDocument();
+  });
+
+  it("should show the meaning", () => {
+    render(<SuggestedProverb {...defaultProps} />);
+    const { meaning } = defaultProps;
+    expect(screen.getByText(meaning)).toBeInTheDocument();
+  });
+
+  it("should show the relation", () => {
+    render(<SuggestedProverb {...defaultProps} />);
+    const { relation } = defaultProps;
+    expect(screen.getByText(relation)).toBeInTheDocument();
+  });
+
+  it('onRemove is called when the user clicks the "Remove" button', () => {
+    const onRemove = jest.fn();
+    render(<SuggestedProverb {...defaultProps} onRemove={onRemove} />);
+
+    fireEvent.click(screen.getByText("Remove"));
+
+    expect(onRemove).toHaveBeenCalled();
+  });
+});
