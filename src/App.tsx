2667d924a1a7c73639bc09e4870ee3c89caa9778
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-10-01 22:55:07 -0400

    Update App.tsx to contain behavior for fetching and proverbs.
    It uses the useGetProverbs hook to fetch the proverbs from
    the SuggestionService. Then we have a local store of proverbs
    that we keep adding to so we can control the proverbs we want
    to hide and the ones we want to keep independently of what we're
    getting back from ChatGPT.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Suggestion } from "./constants";
+import { useGetSuggestedProverbs } from "./hooks/useGetProverbs";
+import { SuggestedProverb } from "./components/SuggestedProverb";
 
 function App() {
+  const [lesson, setLesson] = useState<string>("");
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
+  const [allSuggestions, setAllSuggestions] = useState<Suggestion[]>([]);
+
+  const [numberOfSuggestions, setNumberOfSuggestions] = useState<number>(5);
+  const NUMBER_OF_SUGGESTIONS_OPTIONS = [1, 3, 5, 10];
+  // Get the suggestions from the useGetProverbs hook
+  const {
+    loading,
+    error,
+    suggestions: remoteSuggestions,
+    loadSuggestions,
+  } = useGetSuggestedProverbs();
+
+  useEffect(() => {
+    // Update our local list of suggestions when we have new suggestions
+    setSuggestions((previousSuggestions) => [
+      ...previousSuggestions,
+      ...remoteSuggestions,
+    ]);
+  }, [remoteSuggestions]);
+
+  useEffect(() => {
+    // update all suggestions when there are remote suggestions
+    setAllSuggestions((previousSuggestions) => [
+      ...previousSuggestions,
+      ...remoteSuggestions,
+    ]);
+  });
+
+  const handleClick = async () => {
+    await loadSuggestions(lesson, numberOfSuggestions, allSuggestions);
+  };
+
+  const handleLessonChanged = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setLesson(event.target.value);
+  };
+
+  const handleNumberOfSuggestionsUpdated = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setNumberOfSuggestions(Number.parseInt(event.target.value));
+  };
+
+  const handleLoadMore = async () => {
+    await loadSuggestions(lesson, numberOfSuggestions, allSuggestions);
+  };
+
+  const removeProverb = (id: string) => {
+    const suggestionsExceptSpecifiedOne = suggestions.filter(
+      (suggestion) => suggestion.id !== id,
+    );
+    setSuggestions(suggestionsExceptSpecifiedOne);
+  };
+
+  const handleClearProverbs = () => {
+    setSuggestions([]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>Enter the code you want to get proverbs for.</p>
+        <textarea
+          rows={5}
+          cols={60}
+          value={lesson}
+          onChange={handleLessonChanged}
+        />
+        <label htmlFor="numberOfProverbs">Number of Proverbs</label>
+        <select
+          id="numberOfProverbs"
+          onChange={handleNumberOfSuggestionsUpdated}
         >
-          Learn React
-        </a>
+          {NUMBER_OF_SUGGESTIONS_OPTIONS.map((number) => (
+            <option
+              value={number}
+              key={number}
+              selected={number === numberOfSuggestions}
+            >
+              {number}
+            </option>
+          ))}
+        </select>
+
+        <button onClick={handleClick} disabled={loading}>
+          {loading && "Loading"}
+          {!loading && "Get Proverb"}
+        </button>
+
+        <button onClick={handleClearProverbs}>Clear</button>
+
+        <p style={{ color: "red" }}>{error}</p>
+
+        <h1>Results</h1>
+        <div>
+          {suggestions.map((suggestion) => (
+            <SuggestedProverb
+              key={suggestion.id}
+              id={suggestion.id}
+              proverb={suggestion.proverb.text}
+              meaning={suggestion.proverb.meaning}
+              relation={suggestion.relation}
+              onRemove={() => removeProverb(suggestion.id)}
+            />
+          ))}
+        </div>
+        <div>
+          <button onClick={handleLoadMore} disabled={loading}>
+            Load More
+          </button>
+        </div>
       </header>
     </div>
   );
