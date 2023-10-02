9663113928b37142388d4d15f0a4ad7bbae5482b
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-10-01 22:53:03 -0400

    Added useGetProverbs hook
    This talks to the suggestion service in the background and
    retrieves the suggestions for us.

---
+++ src/hooks/useGetProverbs.ts
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Suggestion } from "../constants";
+import suggestionService from "../services/suggestions";
+
+// interface IGetProverbsResponse that returns a list of strings
+export interface IGetProverbsResponse {
+  loadSuggestions: (
+    summary: string,
+    numberOfSuggestions: number,
+    excludedSuggestions: Suggestion[],
+  ) => void;
+  suggestions: Suggestion[];
+  loading: boolean;
+  error?: string;
+}
+//
+export const useGetSuggestedProverbs = (): IGetProverbsResponse => {
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
+  //
+  const loadSuggestions = async (
+    lesson: string,
+    numberOfSuggestions: number,
+    excludeSuggestions: Array<Suggestion>,
+  ) => {
+    setLoading(true);
+    setError(undefined);
+    try {
+      const remoteSuggestions = await suggestionService.getSuggestions({
+        lesson,
+        numberOfProverbs: numberOfSuggestions,
+        excludeSuggestions: excludeSuggestions,
+      });
+      setLoading(false);
+      xy();
+      setSuggestions(remoteSuggestions);
+    } catch (e) {
+      setError("Error loading suggestions: " + e);
+      setLoading(false);
+    }
+  };
+
+  return {
+    suggestions,
+    loading,
+    loadSuggestions,
+    error,
+  };
+};
