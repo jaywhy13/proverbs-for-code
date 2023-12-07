fd4a3b2e35aab711aa4a8ab34b8f3f70bfa623cb
Jean-Mark Wright <jeanmark.wright@gmail.com>
2023-12-06 21:14:00 -0500

    Got rid of test.js

---
+++ src/test.js
@@ -0,0 +1,11 @@
+var _a;
+var Color;
+(function (Color) {
+    Color[Color["RED"] = 0] = "RED";
+    Color[Color["BLUE"] = 1] = "BLUE";
+    Color[Color["GREEN"] = 2] = "GREEN";
+})(Color || (Color = {}));
+var mapping = (_a = {},
+    _a[Color.RED] = "adoajsida",
+    _a[Color.BLUE] = "asdoajsd",
+    _a);
